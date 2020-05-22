35ad0847acef9bfe588b5208ca0103c0faf08dcb
Erickcamp <thecampkid@gmail.com>
2020-05-22 15:00:36 -0600

    app.js built out

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios'
 import Header from './Components/Header'
+import Functional from './Components/Functional'
 
 class App extends Component{
   constructor(){
@@ -10,13 +11,49 @@ class App extends Component{
       currentTask: []
     }
   }
+
+  componentDidMount = () => {
+    axios.get('/api/task').then((res) => {
+      this.setState({
+        currentTask: res.data
+      })
+    })
+  }
+
+  getTask(name, deadline){
+    const body = {name, deadline}
+    axios.post('/api/task', body).then((res) => {
+      this.setState({
+        currentTask: res.data
+      })
+    })
+  }
+
+  saveTask(id, name, deadline){
+    const body = {name, deadline}
+
+    axios.put(`/api/task/${id}`, body).then((res) => {
+      this.setState({
+        currentTask: res.data
+      })
+    })
+  }
+
+  removeTask(id){
+    axios.delete(`/api/task/${id}`).then((res) => {
+      this.setState({
+        currentTask: res.data
+      })
+    })
+  }
   
   
   render(){
     return (
      <div className="App">
        <Header />
-
+       <Functional />
+      
       </div>
     );
   }
